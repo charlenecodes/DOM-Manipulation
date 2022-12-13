2a9a15d1ad6b33a2b9f8ed5a362cd8175be79388
lava-git <105036972+lava-git@users.noreply.github.com>
2022-12-13 12:13:28 +0100

    style changes

--- index.js
+++ index.js
@@ -2,21 +2,19 @@
 
 let empty = document.querySelector("#empty")
 let url = document.querySelector("#url")
-let button = document.querySelector("#submit")
+let button = document.querySelector("button")
 let submitted = document.querySelector("submitted")
-
+let img = document.createElement("img");
 
 // use javascript to transform the url into an image
 
 function handleSubmit(event) {
-  event.preventDefault();
-  let img = document.createElement("img");
   img.classList.add("photo-style");
   img.src = url.value;
   submitted.appendChild(img);
+  url = "";
   console.log("form was submitted!");
   empty.classList = "hide";
-  url.value = "";
 }
 
 const form = document.querySelector("form");
