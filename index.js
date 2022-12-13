d0bc76953a45550153cf6ce5b52d83301b468cc0
lava-git <105036972+lava-git@users.noreply.github.com>
2022-12-13 11:24:05 +0100

    photo submission not working yet

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
+/* add the variables at the top of the document to make it easier to use within the functions */
+
+let empty = document.querySelector("#empty")
+let url = document.querySelector("#url")
+let button = document.querySelector("#submit")
+let submitted = document.querySelector("submitted")
+
+
+// use javascript to transform the url into an image
 
 function handleSubmit(event) {
+  event.preventDefault();
+  let img = document.createElement("img");
+  img.classList.add("photo-style");
+  img.src = url.value;
+  submitted.appendChild(img);
   console.log("form was submitted!");
+  empty.classList = "hide";
+  url.value = "";
 }
 
 const form = document.querySelector("form");
