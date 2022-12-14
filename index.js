a9a70508b70d0dba62a19d8204dae1f3cd8a35d7
lava-git <105036972+lava-git@users.noreply.github.com>
2022-12-14 09:23:55 +0100

    activity 1 partly completed

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 /* add the variables at the top of the document to make it easier to use within the functions */
 
-let empty = document.querySelector("#empty")
+let empty = document.querySelector(".empty")
 let url = document.querySelector("#url")
 let button = document.querySelector("button")
-let submitted = document.querySelector("submitted")
-let img = document.createElement("img");
+let submitted = document.querySelector("#submitted")
+
 
 // use javascript to transform the url into an image
 
 function handleSubmit(event) {
+  event.preventDefault()
+  let img = document.createElement("img");
   img.classList.add("photo-style");
   img.src = url.value;
   submitted.appendChild(img);
-  url = "";
   console.log("form was submitted!");
+  url.value = "";
   empty.classList = "hide";
 }
 
