810fdf97b651a6e1317ea42ecefb6f02e83a760a
lava-git <105036972+lava-git@users.noreply.github.com>
2022-12-19 15:36:55 +0100

    separated into folders

---
+++ step2/index.js
@@ -0,0 +1,23 @@
+/* add the variables at the top of the document to make it easier to use within the functions */
+
+let empty = document.querySelector(".empty")
+let url = document.querySelector("#url")
+let button = document.querySelector("button")
+let submitted = document.querySelector("#submitted")
+
+
+// use javascript to transform the url into an image
+
+function handleSubmit(event) {
+  event.preventDefault()
+  let img = document.createElement("img");
+  img.classList.add("photo-style");
+  img.src = url.value;
+  submitted.appendChild(img);
+  console.log("form was submitted!");
+  url.value = "";
+  empty.classList = "hide";
+}
+
+const form = document.querySelector("form");
+form.addEventListener("submit", handleSubmit);
